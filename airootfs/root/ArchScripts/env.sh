455c4120690f1634f0065958e16afd00d9708102
Mehradad Shahidi <m.mehrdadshahidi@gmail.com>
2022-12-02 17:21:54 +0330

    🔧 Delete sub-module

---
+++ airootfs/root/ArchScripts/env.sh
@@ -0,0 +1,13 @@
+# Settings for your installed system
+# Hint: To exit vim, press ESC + :wq + ENTER
+
+USERNAME="gabriel"              # Your username
+PASSWORD="arch"                 # Your password
+HOSTNAME="linux-pc"             # The hostname of this particular system
+USERSHELL="/bin/zsh"            # Default shell to be used by root and user
+LANGUAGE="en_US"                # Check /etc/locale.gen for more options
+KEYMAP="us"                     # Check /usr/share/kbd/keymaps/**/*.map.gz for more options
+ZONEINFO="America/Santiago"     # Check /usr/share/zoneinfo/ for more options
+DESKTOP_ENV="kde"               # kde, gnome, i3, x11
+BOOTLOADER="refind"             # refind, grub
+XORG_DRIVERS="nvidia"           # nvidia, amd, vbox, intel, all
