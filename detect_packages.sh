66231df91582d6ed3b092b0c9352b8145997ce8d
Mehrdad Shahidi <m.mehrdadshahidi@gmail.com>
2023-02-13 19:50:07 +0000

    🐎 Ignore arch install packages

--- detect_packages.sh
+++ detect_packages.sh
@@ -3,8 +3,10 @@
 RECIPES_DIR="airootfs/root/ArchScripts/recipes"
 ALL_PACKAGES=""
 
-for recipe_file in $(find ${RECIPES_DIR} -name "*.sh")
-do
-    source ${recipe_file}
-    export ALL_PACKAGES="${ALL_PACKAGES} ${RECIPE_PKGS}"
-done
+# Delete this later
+export ALL_PACKAGES=""
+# for recipe_file in $(find ${RECIPES_DIR} -name "*.sh")
+# do
+#     source ${recipe_file}
+#     export ALL_PACKAGES="${ALL_PACKAGES} ${RECIPE_PKGS}"
+# done
