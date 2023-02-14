c1c11c3e9271e6b92b6a9d32a1ea8fa904dbc3bf
Mehrdad Shahidi <m.mehrdadshahidi@gmail.com>
2023-02-14 08:25:37 +0000

    🐛 Add dummy packages to detect_packages.sh

--- detect_packages.sh
+++ detect_packages.sh
@@ -3,8 +3,8 @@
 RECIPES_DIR="airootfs/root/ArchScripts/recipes"
 ALL_PACKAGES=""
 
-# Delete this later
-export ALL_PACKAGES=""
+# Delete this later, just add one package
+export ALL_PACKAGES="curl"
 # for recipe_file in $(find ${RECIPES_DIR} -name "*.sh")
 # do
 #     source ${recipe_file}
